97b788c669f899611b6feb9467f6f8f8fae4aa35
sushkova <e.isxakova@gmail.com>
2021-11-24 21:31:18 +0300

    Вынесла формы и список в отдельные компоненты

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import {createMenuTemplate} from './view/menu-view.js';
-import {createFiltersTemplate} from './view/filters-view.js';
-import {createSortTemplate} from './view/sort-view.js';
+import { createMenuTemplate } from './view/menu-view.js';
+import { createFiltersTemplate } from './view/filters-view.js';
+import { createSortTemplate } from './view/sort-view.js';
+import { createPointTemplate } from './view/create-point.js';
+import { editPointTemplate } from './view/edit-point.js';
+import { createItemPointTemplate } from './view/list-item.js';
 
 const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -17,7 +20,25 @@ const menuContainer = document.querySelector('.trip-controls__navigation');
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const contentContainer = document.querySelector('.trip-events');
 
-
 renderTemplate(menuContainer, createMenuTemplate(), RenderPosition.AFTERBEGIN)
 renderTemplate(filtersContainer, createFiltersTemplate(), RenderPosition.AFTERBEGIN)
 renderTemplate(contentContainer, createSortTemplate(), RenderPosition.AFTERBEGIN)
+
+contentContainer.insertAdjacentHTML(RenderPosition.BEFOREEND, `<ul class="trip-events__list"></ul>`);
+const listUl = document.querySelector('.trip-events__list');
+
+for (let i = 1; i <= 3; i++) {
+  renderTemplate(listUl, createItemPointTemplate(), RenderPosition.AFTEREND)
+}
+
+const pointListItem = document.querySelector('.trip-events__item');
+
+//форма создания
+renderTemplate(listUl, createPointTemplate(), RenderPosition.AFTERBEGIN)
+
+
+//форма редактирования
+renderTemplate(pointListItem, editPointTemplate(), RenderPosition.BEFOREBEGIN)
+
+
+

645a7eb9d6bd0d5a314a4254ae745516daf0d5b1
sushkova <e.isxakova@gmail.com>
2021-12-17 13:45:06 +0300

    Написала закрытие формы и заглушку

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import SortView from './view/sort-view.js';
 import PointView from './view/point-view.js';
 import EditPointView from './view/edit-point-view.js';
 import PointListView from './view/list-view.js';
+import NoPointView from './view/no-point-view.js';
 import { render, RenderPosition } from './utils.js';
 import { points, destinations, offers } from './mock/point.js';
 
@@ -30,18 +31,40 @@ const renderPoint = (container, point) => {
     pointListView.element.replaceChild(pointView.element, pointEditView.element);
   };
 
+  const onEscKeyDown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.removeEventListener('keydown', onEscKeyDown);
+    }
+  };
+
   pointView.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
     replacePointToForm();
+    document.addEventListener('keydown', onEscKeyDown);
   });
 
   pointEditView.element.querySelector('form').addEventListener('submit', (evt) => {
     evt.preventDefault();
+    replaceFormToPoint();
+    document.addEventListener('keydown', onEscKeyDown);
+  });
+
+  pointEditView.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
     replaceFormToPoint();
   });
 
   render(container, pointView.element, RenderPosition.BEFORE_END);
 };
 
-points.forEach((point) => {
-  renderPoint(pointListView.element, point);
-});
+const renderPointCheck = (points) => {
+  if (points.length === 0) {
+    render(pointListView.element, new NoPointView().element, RenderPosition.BEFORE_END);
+  } else {
+    points.forEach((point) => {
+      renderPoint(pointListView.element, point);
+    });
+    }
+}
+
+renderPointCheck(points)

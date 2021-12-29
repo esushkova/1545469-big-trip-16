6939c20dc27a0da2bebcb2867e69816746bfc603
sushkova <e.isxakova@gmail.com>
2021-12-30 01:04:28 +0300

    Удалила ненужные комментарии

--- src/view/trip-main-info-view.js
+++ src/view/trip-main-info-view.js
@@ -5,28 +5,6 @@ const createTripMainInfoTemplate = () => (
   </section>`
 );
 
-/*
-const createTripMainInfoTemplate = (points) => {
-  let totalPrice = 0;
-
-  for (const point of points) {
-    totalPrice += point['basePrice'];
- }
-
-  return `<section class="trip-main__trip-info  trip-info">
-            <div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-
-              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-            </div>
-
-            <p class="trip-info__cost">
-              Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
-            </p>
-          </section>`
-};
-*/
-
 export default class TripMainInfoView extends AbstractView {
   #points = [];
 
@@ -39,26 +17,3 @@ export default class TripMainInfoView extends AbstractView {
     return createTripMainInfoTemplate(this.#points);
   }
 }
-
-//разделить на три компонента: обертка, div и параграф
-
-/*
-const createTripMainInfoTemplate = () => (
-  `<section class="trip-main__trip-info  trip-info">
-  ${createTripTitleDatesTemplate()}
-  ${createTripCostTemplate()}
-  </section>`
-);
-
-const createTripTitleDatesTemplate = () => (
-  `<div class="trip-info__main">
-    <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
-    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-   </div>`
-);
-
-const createTripCostTemplate = () => (
-  `<p class="trip-info__cost">Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
-   </p>`
-);
-*/

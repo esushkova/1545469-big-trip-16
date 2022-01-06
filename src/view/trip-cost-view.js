2468b9c1652b7e10db757832f0ed7958a79923da
sushkova <e.isxakova@gmail.com>
2022-01-06 14:46:11 +0300

    Добавила стоимость офферов в общую цену поездки

--- src/view/trip-cost-view.js
+++ src/view/trip-cost-view.js
@@ -2,12 +2,22 @@ import AbstractView from './abstract-view.js';
 
 const createTripCostTemplate = (points) => {
   let totalBasePrice = 0;
+  let totalOffersPrice = 0;
+  let totalPrice = 0;
 
   for (const point of points) {
     totalBasePrice += point.basePrice;
+
+    const allOffers = point.offers;
+
+    for(const offer of allOffers) {
+      totalOffersPrice += offer.price;
+    }
   }
 
-  return `<p class="trip-info__cost">Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalBasePrice}</span>
+  totalPrice = totalBasePrice + totalOffersPrice;
+
+  return `<p class="trip-info__cost">Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
    </p>`;
 };
 
